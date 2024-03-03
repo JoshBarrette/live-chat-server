1c77ac70f75ab4623981ba8cca28a3c497adac01
joshbarrette <joshpbarrette@gmail.com>
2024-03-03 17:45:56 -0600

    Hosting 3.0

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
 
   // Need these 2 lines for CORS
   app.useWebSocketAdapter(new SocketAdapter(app));
-  app.enableCors({ origin: true, credentials: true }); // Prob don't need but w/e
+  app.enableCors({ origin: process.env.FRONT_END_URL, credentials: true }); // Prob don't need but w/e
 
   app.setGlobalPrefix("api");
   await app.listen(process.env.PORT || 4000);
